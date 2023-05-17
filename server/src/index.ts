d938894bdefba2f3ff9032f1051c6c7329f572b1
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-05-17 11:54:05 +0700

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import Hapi, { Server } from '@hapi/hapi';
+
+const init = async function () {
+  const server: Server = Hapi.server({
+    port: process.env.PORT || 8000,
+    host: '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+init();
