eca2ee0d56eb3aa98078354909c1ac0cdcfa1cae
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-05-20 18:07:22 +0700

    auth

---
+++ server/src/constant/firebase.ts
@@ -0,0 +1,10 @@
+export const FIREBASE = {
+  Credential: '',
+  APP_ID: '',
+  AUTH_DOMAIN: '',
+  DATABASE_URL: '',
+  MEASUREMENT_ID: '',
+  MESSAGING_SENDER_ID: '',
+  PROJECT_ID: '',
+  STORAGE_BUCKET: '',
+};
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,14 @@
 /* eslint-disable import/prefer-default-export */
 import { ReqRefDefaults, ServerRoute } from '@hapi/hapi';
 import { hellowordHandler } from './handler/helloworld';
+import { authHandler } from './handler/authHandler';
 
 export const route: ServerRoute<ReqRefDefaults>[] = [{
   method: 'GET',
-  path: '/',
-  handler: hellowordHandler
+  path: '/api',
+  handler: hellowordHandler,
+},{
+  method: 'POST',
+  path: '/api/authentication',
+  handler: authHandler,
 }];
