346d92484360064faa2f178cda93a58e60cc6317
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-06-05 20:36:02 +0700

    delete notif

--- server/src/api/handler/user/notifications.ts
+++ server/src/api/handler/user/notifications.ts
@@ -25,3 +25,25 @@ export const getNotifications = async (
     throw Boom.badGateway('');
   }
 };
+
+export const deleteNotification = async (
+  request: Request<ReqRefDefaults>,
+  h: ResponseToolkit<ReqRefDefaults>,
+) => {
+  try {
+    const user = request.pre.user as User;
+    const notificationId = Number(request.params.notificationId);
+    return await db.notification.deleteMany({
+      where: {
+        id: notificationId,
+        user_id: user.id,
+      },
+    });
+  } catch (error) {
+    if (Boom.isBoom(error)) {
+      throw error;
+    }
+    request.log('error', error); // unexpected error
+    throw Boom.badGateway('');
+  }
+};
