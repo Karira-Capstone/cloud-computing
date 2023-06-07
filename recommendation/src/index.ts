9e54d2dbf8b3610d0a57177603a0a897c8096b38
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-06-07 18:27:21 +0700

    change send

--- recommendation/src/index.ts
+++ recommendation/src/index.ts
@@ -2,35 +2,41 @@ import { PUBSUB_CONFIG, createSubscription, pubSubClient } from "./google/pubsub
 import { projectCreatedHandler } from "./handler/project-created";
 import { serviceCreatedHandler } from "./handler/service-created";
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/recommendation/hello", (req, res)=>{
-  return "Hello, world!";
-})
-
-app.post("/api/recommendation/project", async (req, res) => {
-  await projectCreatedHandler(req.body);
+app.get("/api/recommendation/hello", (req, res) => {
+  res.send("Hello World");
 });
 
 app.post("/api/recommendation/project", async (req, res) => {
-  await serviceCreatedHandler(req.body);
+  try {
+    await projectCreatedHandler(req.body);
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/api/recommendation/service", async (req, res) => {
+  try {
+    await serviceCreatedHandler(req.body);
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 const init = async () => {
   app.listen(8000, async () => {
     console.log("Open on 8000");
   });
-  // const projectcreatedSubscription = pubSubClient.subscription(PUBSUB_CONFIG.SUBSCRIPTION.PROJECT_CREATED);
-  // projectcreatedSubscription.on('message', projectCreatedHandler);
-
-  // const serviceCreatedSubscription = pubSubClient.subscription(PUBSUB_CONFIG.SUBSCRIPTION.SERVICE_CREATED);
-  // serviceCreatedSubscription.on('message', serviceCreatedHandler);
-  // console.log("Started")
 };
 
 init();
