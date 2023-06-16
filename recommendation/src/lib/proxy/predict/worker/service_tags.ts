6543d54ecf46a49dfa37b4bfa799388c134405bf
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-06-16 09:25:29 +0700

    update input key

--- recommendation/src/lib/proxy/predict/worker/service_tags.ts
+++ recommendation/src/lib/proxy/predict/worker/service_tags.ts
@@ -2,7 +2,7 @@ import { MLPredictProxyClass } from "../../MLProxy";
 
 class MLProxyPredictServiceTagsClass extends MLPredictProxyClass {
   modelname = "service_tags";
-  input_key = "input_2";
+  input_key = "input_1";
 }
 
 export const MLProxyPredictServiceTags = new MLProxyPredictServiceTagsClass()
